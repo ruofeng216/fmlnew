647b22030873953f317b5d5889d6c0e013980619
tujiaw <tujiaw@163.com>
2017-03-12 15:00:06 +0800

    写日志效率优化

--- FML/include/util/log.h
+++ FML/include/util/log.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include <QtDebug>
+#include <QDebug>
+#include <QMutex>
+#include <QMutexLocker> 
+#include <QSettings>
+#include <QDate>
+#include <QThread>
+#include "util/util.h"
 
-
-
-static int m_nlog = 0;
 enum LogLevel
 {
 	LL_TRACE,
@@ -16,13 +20,77 @@ enum LogLevel
 	LOGLEVEL_COUNT
 };
 
-#define LOG_TRACE if (m_nlog <= LL_TRACE) \
-	qDebug()
-#define LOG_DEBUG if (m_nlog <= LL_DEBUG) \
-	qDebug()
-#define LOG_INFO if (m_nlog <= LL_INFO) \
-	qInfo()
-#define LOG_WARN if (m_nlog <= LL_WARN) \
-	qWarning()
-#define LOG_ERROR qCritical()
-#define LOG_FATAL qFatal()
+#define _TIME_ qPrintable(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss.zzz"))
+
+static struct LogWrap {
+	LogWrap::LogWrap() { reset(); }
+	LogWrap::~LogWrap() { reset(); }
+	void reset() {
+		isInit = false;
+		level = "";
+		date = "";
+		if (ts) {
+			delete ts;
+			ts = nullptr;
+		}
+		if (file) {
+			delete file;
+			file = nullptr;
+		}
+	}
+	bool isInit;
+	QMutex locker;
+	QString level;
+	QString date;
+	QFile *file;
+	QTextStream *ts;
+} s_logWrap;
+static QStringList logStr = QStringList() << "TRACE" << "DEBUG" << "INFO" << "WARN" << "ERROR" << "FATAL";
+
+//回调函数实现debug信息到文件
+void customMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+	LogLevel _log;
+	switch (type) {
+	case QtInfoMsg:
+		_log = LL_INFO;
+	case QtDebugMsg:
+		_log = LL_DEBUG;
+		break;
+	case QtWarningMsg:
+		_log = LL_WARN;
+		break;
+	case QtCriticalMsg:
+		_log = LL_ERROR;
+		break;
+	case QtFatalMsg:
+		_log = LL_FATAL;
+		abort();
+	default:
+		_log = LL_TRACE;
+	}
+
+	QString curDate = QDate::currentDate().toString("yyyyMMdd");
+	if (curDate != s_logWrap.date) {
+		s_logWrap.reset();
+	}
+
+	if (!s_logWrap.isInit) {
+		s_logWrap.isInit = true;
+		QDir().mkpath(qutil::logDir());
+		QSettings configIniRead(qutil::qBaseConfigPath(), QSettings::IniFormat);
+		s_logWrap.level = configIniRead.value("/Base/loglevel").toString().toUpper();
+		s_logWrap.date = QDate::currentDate().toString("yyyyMMdd");
+		s_logWrap.file = new QFile(qutil::logDir() + QString("\\%1.log").arg(s_logWrap.date));
+		if (s_logWrap.file->open(QIODevice::WriteOnly | QIODevice::Append)) {
+			s_logWrap.ts = new QTextStream(s_logWrap.file);
+		}
+	}
+
+	if (s_logWrap.ts) {
+		QString text = _TIME_ + QString("[%1:%2:%3]").arg(context.file).arg(context.function).arg(context.line) + QString("[%1][%2]%3\n").arg(logStr[_log]).arg((qint64)QThread::currentThreadId()).arg(msg);
+		OutputDebugString(text.toStdWString().c_str());
+		QMutexLocker locker(&s_logWrap.locker);
+		(*s_logWrap.ts << text).flush();
+	}
+}
--- FML/main.cpp
+++ FML/main.cpp
@@ -1,64 +1,7 @@
 #include <QtWidgets/QApplication>
 #include "qmpreload.h"
 #include <QCoreApplication>
-#include <QDebug>
-#include <QFile>
-#include <QTextStream>
-#include <QMutex>
-#include <QMutexLocker> 
-#include <QSettings>
-#include <QDate>
-#include <QThread>
-#include <QDir>
 #include "util/log.h"
-#include "util/util.h"
-
-
-#define _TIME_ qPrintable(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss.zzz"))
-
-static QMutex m_gLocker;
-static QString m_loglevel = "";
-//回调函数实现debug信息到文件
-void customMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
-{
-	QStringList logStr = QStringList() << "TRACE" << "DEBUG" << "INFO" << "WARN" << "ERROR" << "FATAL";
-	LogLevel _log;
-	if (m_loglevel.isEmpty())
-	{
-		QSettings configIniRead(qutil::qBaseConfigPath(), QSettings::IniFormat);
-		m_loglevel = configIniRead.value("/Base/loglevel").toString().toUpper();
-		m_nlog = logStr.indexOf(m_loglevel);
-	}
-
-	switch (type) {
-	case QtInfoMsg:
-		_log = LL_INFO;
-	case QtDebugMsg:
-		_log = LL_DEBUG;
-		break;
-	case QtWarningMsg:
-		_log = LL_WARN;
-		break;
-	case QtCriticalMsg:
-		_log = LL_ERROR;
-		break;
-	case QtFatalMsg:
-		_log = LL_FATAL;
-		abort();
-	default:
-		_log = LL_TRACE;
-	}
-	QMutexLocker locker(&m_gLocker);
-	QDir().mkpath(qutil::logDir());
-	QFile outFile(qutil::logDir() + QString("\\%1.log").arg(QDate::currentDate().toString("yyyyMMdd")));
-	if (outFile.open(QIODevice::WriteOnly | QIODevice::Append))
-	{
-		QTextStream ts(&outFile);
-		QString text = _TIME_ + QString("[%1:%2:%3]").arg(context.file).arg(context.function).arg(context.line) + QString("[%1][%2]%3\n").arg(logStr[_log]).arg((qint64)QThread::currentThreadId()).arg(msg);
-		ts << text;
-		OutputDebugString(text.toStdWString().c_str());
-	}
-}
 
 int main(int argc, char *argv[])
 {
